c409438aefb109a1672c563b3081fb58dcd834c4
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2024-07-29 14:46:59 +0700

    #3 - Booking and Menu table model and Migrations

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee@123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -116,12 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'restaurant/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static/",
-    'restaurant/'
-]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Booking Table
+class BookingTable (models.Model):
+    name        = models.CharField(max_length=255)
+    noOfGuests  = models.IntegerField()
+    bookingDate = models.DateField()
+
+# Menu Table
+class MenuTable (models.Model):
+    title       = models.CharField(max_length=255)
+    price       = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory   = models.IntegerField()
